e439cf52e937be7031878d65fb35636d8a67f473
Javiyack <javillach@gmail.com>
2018-11-18 08:03:38 +0100

    Repasado hasta nivel A. Faltan tests

--- create Acme users.sql
+++ create Acme users.sql
@@ -1,6 +1,16 @@
 start transaction;
+USE `Acme-Showroom`;
 
 create user 'acme-user'@'%' identified by password '*4F10007AADA9EE3DBB2CC36575DFC6F4FDE27577';
 create user 'acme-manager'@'%' identified by password '*FDB8CD304EB2317D10C95D797A4BD7492560F55F';
+
+
+GRANT SELECT, INSERT, UPDATE, DELETE
+  ON `Acme-Showroom`.* TO 'acme-user'@'%';
+
+GRANT SELECT, INSERT, UPDATE, DELETE, CREATE, DROP, REFERENCES, INDEX, ALTER,
+    CREATE TEMPORARY TABLES, LOCK TABLES, CREATE VIEW, CREATE ROUTINE,
+    ALTER ROUTINE, EXECUTE, TRIGGER, SHOW VIEW
+  ON `Acme-Showroom`.* TO 'acme-manager'@'%';
 
 commit;
